a0c700535623c308b0727b61f113ecf67335af67
gaurav1996b <1996gauravb@gmail.com>
2022-08-01 16:23:39 +0530

    updating terraform file with roboshop project

---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "ENGINE" {}
+variable "ENGINE_VERSION" {}
+variable "INSTANCE_CLASS" {}
+variable "PG_FAMILY" {}
+variable "PRIVATE_SUBNET_IDS" {}
+variable "VPC_ID" {}
+variable "PORT" {}
+variable "ALLOW_SG_CIDR" {}
+variable "PROJECT" {}
+variable "ENV" {}
